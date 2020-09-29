6636a9e88c75eab51bf95b1009ffa58bd637ff2a
Yevhenko <yevhenkozlyi@gmail.com>
2020-09-29 15:28:06 +0300

    homework committed

--- index.js
+++ index.js
@@ -1,24 +1,110 @@
-// Create a function that receives an array of goods and returns cost that should be paid for all these goods.
-// Your code should:
-// - Print to the console the total quantity of pairs of socks (`Socks - ${quantity}`);
-// - Print to the console the total quantity of the hats of red color (`Red Hats - ${quantity}`);
-// - Print to the console the cost of the goods by colors (`Red - ${costRed}, Green - ${costGreen}, Blue - ${costBlue}`);
-// - Print to the console the result of the execution of this function.
+let socksQuantity = 0;
+let redHatsQuantity = 0;
+let costRed = 0;
+let costGreen = 0;
+let costBlue = 0;
+let totalCost = 0;
 
-// Note: Try to minimize your code.
-// Input array in JSON format:
-[
-    {"type": "socks", "color": "red", "quantity": 10, "priceForPair": "$3"},
-    {"type": "socks", "color": "green", "quantity": 5, "priceForPair": "$10"},
-    {"type": "socks", "color": "blue", "quantity": 8, "priceForPair": "$6"},
-    {"type": "hat", "color": "red", "quantity": 7, "price": "$5"},
-    {"type": "hat", "color": "green", "quantity": 0, "price": "$6"},
-    {"type": "socks", "color": "blue", "priceForPair": "$6"},
-    {"type": "socks", "color": "red", "quantity": 10, "priceForPair": "$3"},
-    {"type": "socks", "color": "white", "quantity": 3, "priceForPair": "$4"},
-    {"type": "socks", "color": "green", "priceForPair": "$10"},
-    {"type": "socks", "color": "blue", "quantity": 2, "priceForPair": "$6"},
-    {"type": "hat", "color": "green", "quantity": 3, "price": "$5"},
-    {"type": "hat", "color": "red", "quantity": 1, "price": "$6"},
-    {"type": "socks", "color": "blue", "priceForPair": "$6"}
-]
+const regexp = new RegExp(/\d+/);
+
+function arrayHandler() {
+  const array = [
+    {
+      type: 'socks',
+      color: 'red',
+      quantity: 10,
+      priceForPair: '$3',
+    },
+    {
+      type: 'socks',
+      color: 'green',
+      quantity: 5,
+      priceForPair: '$10',
+    },
+    {
+      type: 'socks',
+      color: 'blue',
+      quantity: 8,
+      priceForPair: '$6',
+    },
+    {
+      type: 'hat',
+      color: 'red',
+      quantity: 7,
+      price: '$5',
+    },
+    {
+      type: 'hat',
+      color: 'green',
+      quantity: 0,
+      price: '$6',
+    },
+    { type: 'socks', color: 'blue', priceForPair: '$6' },
+    {
+      type: 'socks',
+      color: 'red',
+      quantity: 10,
+      priceForPair: '$3',
+    },
+    {
+      type: 'socks',
+      color: 'white',
+      quantity: 3,
+      priceForPair: '$4',
+    },
+    { type: 'socks', color: 'green', priceForPair: '$10' },
+    {
+      type: 'socks',
+      color: 'blue',
+      quantity: 2,
+      priceForPair: '$6',
+    },
+    {
+      type: 'hat',
+      color: 'green',
+      quantity: 3,
+      price: '$5',
+    },
+    {
+      type: 'hat',
+      color: 'red',
+      quantity: 1,
+      price: '$6',
+    },
+    { type: 'socks', color: 'blue', priceForPair: '$6' },
+  ];
+
+  for (let i = 0; i < array.length; i += 1) {
+    if (array[i].type === 'socks' && array[i].quantity) {
+      socksQuantity += array[i].quantity;
+    }
+
+    if (array[i].type === 'hat' && array[i].color === 'red' && array[i].quantity) {
+      redHatsQuantity += array[i].quantity;
+    }
+
+    if (array[i].color === 'red' && (array[i].price || array[i].priceForPair)) {
+      costRed += Number(regexp.exec(array[i].price + array[i].priceForPair));
+    }
+
+    if (array[i].color === 'green' && (array[i].price || array[i].priceForPair)) {
+      costGreen += Number(regexp.exec(array[i].price + array[i].priceForPair));
+    }
+
+    if (array[i].color === 'blue' && (array[i].price || array[i].priceForPair)) {
+      costBlue += Number(regexp.exec(array[i].price + array[i].priceForPair));
+    }
+
+    if (array[i].price || array[i].priceForPair) {
+      totalCost += Number(regexp.exec(array[i].price + array[i].priceForPair));
+    }
+  }
+  console.log(`Socks - ${socksQuantity}`);
+  console.log(`Socks - ${redHatsQuantity}`);
+  console.log(`Red - ${costRed}`);
+  console.log(`Green - ${costGreen}`);
+  console.log(`Blue - ${costBlue}`);
+  return `Total cost is - ${totalCost}`;
+}
+
+console.log(arrayHandler());

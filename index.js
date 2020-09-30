33c67de77dddb14dc3aa241dbb98bd670891e8cf
Yevhenko <yevhenkozlyi@gmail.com>
2020-09-30 12:19:37 +0300

    some stuff fixed

--- index.js
+++ index.js
@@ -1,3 +1,71 @@
+const array = [
+  {
+    type: 'socks',
+    color: 'red',
+    quantity: 10,
+    priceForPair: '$3',
+  },
+  {
+    type: 'socks',
+    color: 'green',
+    quantity: 5,
+    priceForPair: '$10',
+  },
+  {
+    type: 'socks',
+    color: 'blue',
+    quantity: 8,
+    priceForPair: '$6',
+  },
+  {
+    type: 'hat',
+    color: 'red',
+    quantity: 7,
+    price: '$5',
+  },
+  {
+    type: 'hat',
+    color: 'green',
+    quantity: 0,
+    price: '$6',
+  },
+  { type: 'socks', color: 'blue', priceForPair: '$6' },
+  {
+    type: 'socks',
+    color: 'red',
+    quantity: 10,
+    priceForPair: '$3',
+  },
+  {
+    type: 'socks',
+    color: 'white',
+    quantity: 3,
+    priceForPair: '$4',
+  },
+  { type: 'socks', color: 'green', priceForPair: '$10' },
+  {
+    type: 'socks',
+    color: 'blue',
+    quantity: 2,
+    priceForPair: '$6',
+  },
+  {
+    type: 'hat',
+    color: 'green',
+    quantity: 3,
+    price: '$5',
+  },
+  {
+    type: 'hat',
+    color: 'red',
+    quantity: 1,
+    price: '$6',
+  },
+  { type: 'socks', color: 'blue', priceForPair: '$6' },
+];
+
+const regexp = new RegExp(/\d+/);
+
 let socksQuantity = 0;
 let redHatsQuantity = 0;
 let costRed = 0;
@@ -5,75 +73,7 @@ let costGreen = 0;
 let costBlue = 0;
 let totalCost = 0;
 
-const regexp = new RegExp(/\d+/);
-
 function arrayHandler() {
-  const array = [
-    {
-      type: 'socks',
-      color: 'red',
-      quantity: 10,
-      priceForPair: '$3',
-    },
-    {
-      type: 'socks',
-      color: 'green',
-      quantity: 5,
-      priceForPair: '$10',
-    },
-    {
-      type: 'socks',
-      color: 'blue',
-      quantity: 8,
-      priceForPair: '$6',
-    },
-    {
-      type: 'hat',
-      color: 'red',
-      quantity: 7,
-      price: '$5',
-    },
-    {
-      type: 'hat',
-      color: 'green',
-      quantity: 0,
-      price: '$6',
-    },
-    { type: 'socks', color: 'blue', priceForPair: '$6' },
-    {
-      type: 'socks',
-      color: 'red',
-      quantity: 10,
-      priceForPair: '$3',
-    },
-    {
-      type: 'socks',
-      color: 'white',
-      quantity: 3,
-      priceForPair: '$4',
-    },
-    { type: 'socks', color: 'green', priceForPair: '$10' },
-    {
-      type: 'socks',
-      color: 'blue',
-      quantity: 2,
-      priceForPair: '$6',
-    },
-    {
-      type: 'hat',
-      color: 'green',
-      quantity: 3,
-      price: '$5',
-    },
-    {
-      type: 'hat',
-      color: 'red',
-      quantity: 1,
-      price: '$6',
-    },
-    { type: 'socks', color: 'blue', priceForPair: '$6' },
-  ];
-
   for (let i = 0; i < array.length; i += 1) {
     if (array[i].type === 'socks' && array[i].quantity) {
       socksQuantity += array[i].quantity;
@@ -83,28 +83,31 @@ function arrayHandler() {
       redHatsQuantity += array[i].quantity;
     }
 
-    if (array[i].color === 'red' && (array[i].price || array[i].priceForPair)) {
-      costRed += Number(regexp.exec(array[i].price + array[i].priceForPair));
+    const price = array[i].price || array[i].priceForPair;
+    const commonPrice = array[i].price + array[i].priceForPair;
+
+    if (array[i].color === 'red' && price) {
+      costRed += Number(regexp.exec(commonPrice));
     }
 
-    if (array[i].color === 'green' && (array[i].price || array[i].priceForPair)) {
-      costGreen += Number(regexp.exec(array[i].price + array[i].priceForPair));
+    if (array[i].color === 'green' && price) {
+      costGreen += Number(regexp.exec(commonPrice));
     }
 
-    if (array[i].color === 'blue' && (array[i].price || array[i].priceForPair)) {
-      costBlue += Number(regexp.exec(array[i].price + array[i].priceForPair));
+    if (array[i].color === 'blue' && price) {
+      costBlue += Number(regexp.exec(commonPrice));
     }
 
-    if (array[i].price || array[i].priceForPair) {
-      totalCost += Number(regexp.exec(array[i].price + array[i].priceForPair));
+    if (price) {
+      totalCost += Number(regexp.exec(commonPrice));
     }
   }
   console.log(`Socks - ${socksQuantity}`);
-  console.log(`Socks - ${redHatsQuantity}`);
-  console.log(`Red - ${costRed}`);
-  console.log(`Green - ${costGreen}`);
-  console.log(`Blue - ${costBlue}`);
-  return `Total cost is - ${totalCost}`;
+  console.log(`Red hats - ${redHatsQuantity}`);
+  console.log(`Red items - ${costRed}`);
+  console.log(`Green items - ${costGreen}`);
+  console.log(`Blue items - ${costBlue}`);
+  return `Total cost of all items is - ${totalCost}`;
 }
 
 console.log(arrayHandler());

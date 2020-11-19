64c721c8a73afa624e6ce8595f59480660fdece3
Yevhenko <yevhenkozlyi@gmail.com>
2020-11-19 15:39:07 +0200

    one more commit

--- homework-02/router.js
+++ homework-02/router.js
@@ -8,7 +8,7 @@ module.exports = (request, response) => {
   // eslint-disable-next-line object-curly-newline
   const { method, url, queryParams, body } = request;
 
-  if (method === 'GET' && url.pathname === '/task1' && queryParams) getTask1(response, queryParams);
+  if (method === 'GET' && url.pathname === '/task1') getTask1(response, queryParams);
   else if (method === 'GET' && url.pathname === '/task2') getTask2(response);
   else if (method === 'GET' && url.pathname === '/task3') getTask3(response);
   else if (method === 'POST' && url.pathname === '/newArray') setNewArray(response, body);

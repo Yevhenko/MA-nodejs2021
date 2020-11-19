38d23eccc4398f47a2d780905fca66bc7107003d
Yevhenko <yevhenkozlyi@gmail.com>
2020-11-19 15:48:01 +0200

    one more commit

--- homework-02/controller/task1Handler.js
+++ homework-02/controller/task1Handler.js
@@ -4,8 +4,12 @@ const array = require('../clothes.json');
 
 function getTask1(response, queryParams) {
   try {
-    const data = filterByKey(array, JSON.stringify(queryParams));
-    success(response, data);
+    if (Object.keys(queryParams).length === 0) {
+      response.writeHead(500).end();
+    } else {
+      const data = filterByKey(array, JSON.stringify(queryParams));
+      success(response, data);
+    }
   } catch (error) {
     response.writeHead(500).end();
   }

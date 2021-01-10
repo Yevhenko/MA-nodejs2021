463c5017288d8f5edd2b17305e2fab253c23b686
Yevhenko <yevhenkozlyi@gmail.com>
2021-01-10 16:11:03 +0200

    middlewares fixed

--- homework-08/src/server/routes/products.js
+++ homework-08/src/server/routes/products.js
@@ -11,28 +11,28 @@ const {
 
 const products = express.Router();
 
-products.post('/products', async (req, res) => {
+products.post('/products', async (req, res, next) => {
   try {
     const { body } = req;
 
     if (!body) {
-      return res.status(404).send('Not found');
+      res.status(404).send('Not found');
     }
     const response = await createProduct(body);
 
-    return res.send(response);
+    res.send(response);
   } catch (error) {
     console.error(error);
-    return res.status(500).send('The product cannot be created');
+    next(error);
   }
 });
 
 products.post('/db/push/:filename', fromJSONtoDB);
 
-products.get('/products', async (req, res) => {
+products.get('/products', async (req, res, next) => {
   try {
     const { query } = req;
-    const idOfProduct = parseInt(req.query.id, 10);
+    const idOfProduct = Number(req.query.id, 10);
 
     if (!idOfProduct) {
       res.status(404).send('Not found');
@@ -41,44 +41,41 @@ products.get('/products', async (req, res) => {
     const gotProducts = await getProduct(query);
     res.send(gotProducts);
   } catch (error) {
-    console.error(error);
-    res.status(500).send('The product cannot be got');
+    next(error);
   }
 });
 
-products.put('/products', async (req, res) => {
+products.put('/products', async (req, res, next) => {
   try {
     const { body, query } = req;
-    const idOfProduct = parseInt(req.query.id, 10);
+    const idOfProduct = Number(req.query.id, 10);
 
     if (!idOfProduct) {
-      return res.status(404).send('Not found');
+      res.status(404).send('Not found');
     }
 
     await updateProduct(body, query);
 
-    return res.end('Product updated!');
+    res.end('Product updated!');
   } catch (error) {
-    console.error(error);
-    return res.status(500).send('The product cannot be updated');
+    next(error);
   }
 });
 
-products.delete('/products', async (req, res) => {
+products.delete('/products', async (req, res, next) => {
   try {
     const { query } = req;
-    const idOfProduct = parseInt(req.query.id, 10);
+    const idOfProduct = Number(req.query.id, 10);
 
     if (!idOfProduct) {
-      return res.status(404).send('Not found');
+      res.status(404).send('Not found');
     }
 
     await deleteProduct(query);
 
-    return res.end('Product deleted!');
+    res.end('Product deleted!');
   } catch (error) {
-    console.error(error);
-    return res.status(500).send('The product cannot be deleted at the moment');
+    next(error);
   }
 });
 

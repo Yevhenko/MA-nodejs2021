ab24d124acfc4910d9f50cea37526aebc6d3a4ce
Yevhenko <yevhenkozlyi@gmail.com>
2021-01-10 15:56:34 +0200

    middlewares fixed

--- homework-06/src/server/routes/index.js
+++ homework-06/src/server/routes/index.js
@@ -4,6 +4,9 @@ const products = require('./products');
 
 const router = express.Router();
 
+const { errorHandler } = require('../../utils/errorHandler');
+
 router.use(products);
+router.use(errorHandler);
 
 module.exports = router;

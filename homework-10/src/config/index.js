a73033ea306f687510fa7ea59d1b703b6d052bef
Yevhenko <yevhenkozlyi@gmail.com>
2021-01-16 13:49:04 +0200

    initial commit of homework-10

---
+++ homework-10/src/config/index.js
@@ -0,0 +1,31 @@
+require('dotenv').config();
+
+const { fatal } = require('../utils/fatal');
+
+const config = {
+  port: process.env.PORT || 3000,
+  db: {
+    config: {
+      knex: {
+        client: 'pg',
+        connection: {
+          user: process.env.DB_USER || fatal('user not defined'),
+          host: process.env.DB_HOST || fatal('host not defined'),
+          port: process.env.DB_PORT || fatal('port not defined'),
+          database: process.env.DB_NAME || fatal('db not defined'),
+          password: process.env.DB_PASS || fatal('password not defined'),
+        },
+        pool: {
+          min: 2,
+          max: 10,
+        },
+        debug: false,
+        migrations: {
+          tableName: 'knex_migrations',
+        },
+      },
+    },
+  },
+};
+
+module.exports = config;
